8ee62cb503d6ee56a6282c93dcc627350448647e
Jiddang <Jiddang@192.168.219.100>
2021-10-27 22:59:22 +0900

    2021/10/27 22:00 commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,23 +3,33 @@
  */
 
 $(document).ready(function() {
-	/*$('#fullpage').fullpage({
-		//options here
-		autoScrolling:true,
-		scrollHorizontally: true
-	});
-
-	//methods
-	$.fn.fullpage.setAllowScrolling(false);*/
-	
 	$('#fullpage').fullpage({
-		//이동
-		menu: '#menu',
-		lockAnchors: false,
-		anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+		//move
 		navigation: true,
 		navigationPosition: 'right',
 		navigationTooltips: ['BLAN', 'who is BLAN', 'we with Artist', 'to work on a Project'],
+
+		//scroll
+		autoScrolling: true,
+		scrollHorizontally: true,	//version 3
+
+		//access
+
+		//design
+		sectionsColor: ['#aaa', '#bbb', '#ccc', '#ddd']
+	});
+
+	/*//methods
+	$.fn.fullpage.setAllowScrolling(false);*/
+
+	/*$('#fullpage').fullpage({
+		//이동
+		menu: '#menu',
+		lockAnchors: false,
+		anchors: ['firstPage', 'secondPage'],
+		navigation: false,
+		navigationPosition: 'right',
+		navigationTooltips: ['firstSlide', 'secondSlide'],
 		showActiveTooltip: false,
 		slidesNavigation: false,
 		slidesNavPosition: 'bottom',
@@ -59,7 +69,7 @@ $(document).ready(function() {
 		//디자인
 		controlArrows: true,
 		verticalCentered: true,
-		sectionsColor : ['#ccc', '#ddd', '#aaa', '#bbb'],
+		sectionsColor: ['#ccc', '#fff'],
 		paddingTop: '3em',
 		paddingBottom: '10px',
 		fixedElements: '#header, .footer',
@@ -67,28 +77,27 @@ $(document).ready(function() {
 		responsiveHeight: 0,
 		responsiveSlides: false,
 		parallax: false,
-		parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+		parallaxOptions: { type: 'reveal', percentage: 62, property: 'translate' },
 		dropEffect: false,
-		dropEffectOptions: { speed: 2300, color: '#F82F4D', zIndex: 9999},
+		dropEffectOptions: { speed: 2300, color: '#F82F4D', zIndex: 9999 },
 		waterEffect: false,
-		waterEffectOptions: { animateContent: true, animateOnMouseMove: true},
+		waterEffectOptions: { animateContent: true, animateOnMouseMove: true },
 		cards: false,
-		cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},
+		cardsOptions: { perspective: 100, fadeContent: true, fadeBackground: true },
 
 		//맞춤 선택자
 		sectionSelector: '.section',
 		slideSelector: '.slide',
 
 		lazyLoading: true,
-		
 		//사건(이벤트)
-		onLeave: function(origin, destination, direction){},
-		afterLoad: function(origin, destination, direction){},
-		afterRender: function(){},
-		afterResize: function(width, height){},
-		afterReBuild: function(){},
-		afterResponsive: function(isResponsive){},
-		afterSlideLoad: function(section, origin, destination, direction){},
-		onSlideLeave: function(section, origin, destination, direction){}
-	});
+		onLeave: function(origin, destination, direction) { },
+		afterLoad: function(origin, destination, direction) { },
+		afterRender: function() { },
+		afterResize: function(width, height) { },
+		afterReBuild: function() { },
+		afterResponsive: function(isResponsive) { },
+		afterSlideLoad: function(section, origin, destination, direction) { },
+		onSlideLeave: function(section, origin, destination, direction) { }
+	});*/
 });
